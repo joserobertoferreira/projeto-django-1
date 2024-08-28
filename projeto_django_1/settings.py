f44049a719ed530948254050a2e23d3234662907
joserobertoferreira <joseroberto.ferreira@gmail.com>
2024-08-28 21:03:45 +0100

    Django admin

--- projeto_django_1/settings.py
+++ projeto_django_1/settings.py
@@ -124,6 +124,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'resources',
 ]
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 
+from recipes.models import Recipe
 from resources.utils.factory import make_recipe
 
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+
     return render(
         request,
         'recipes/pages/home.html',
         context={
-            'recipes': [make_recipe() for _ in range(10)],
+            'recipes': recipes,
         },
     )
 
@@ -18,4 +21,18 @@ def recipe(request, id):
         request,
         'recipes/pages/recipe-view.html',
         context={'recipe': make_recipe(), 'is_detail_page': True},
+    )
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True
+    ).order_by('-id')
+
+    return render(
+        request,
+        'recipes/pages/category.html',
+        context={
+            'recipes': recipes,
+        },
     )
