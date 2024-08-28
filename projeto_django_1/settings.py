039d5a11c280c5758995fa87f32f4d1a703829b9
joserobertoferreira <joseroberto.ferreira@gmail.com>
2024-08-28 19:23:44 +0100

    Criação de modelos e migrações

--- projeto_django_1/settings.py
+++ projeto_django_1/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'projeto_django_1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'resources', BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'resources',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
