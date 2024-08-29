cc339e246b8f7b9d05da5668802f71b6bdd6083e
joserobertoferreira <joseroberto.ferreira@gmail.com>
2024-08-29 12:16:05 +0100

    Page 404

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
-from resources.utils.factory import make_recipe
 
 
 def home(request):
@@ -17,22 +16,27 @@ def home(request):
 
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
+
     return render(
         request,
         'recipes/pages/recipe-view.html',
-        context={'recipe': make_recipe(), 'is_detail_page': True},
+        context={'recipe': recipe, 'is_detail_page': True},
     )
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True
-    ).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id, is_published=True
+        ).order_by('-id')
+    )
 
     return render(
         request,
         'recipes/pages/category.html',
         context={
             'recipes': recipes,
+            'title': f'Categoria - {recipes[0].category.name} |',
         },
     )
