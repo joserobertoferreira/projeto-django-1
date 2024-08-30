6957d952aaa7fe60c64151a3518afcefebdd362d
joserobertoferreira <joseroberto.ferreira@gmail.com>
2024-08-30 22:29:45 +0100

    Coverage tests

---
+++ tests/test_recipe_base.py
@@ -0,0 +1,63 @@
+from django.test import TestCase
+
+from recipes.models import Category, Recipe, User
+
+
+class RecipeBaseTest(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def create_category(self, name='Outros') -> Category:  # noqa: PLR6301
+        return Category.objects.create(name=name)
+
+    def create_author(  # noqa: PLR6301
+        self,
+        first_name='John',
+        last_name='Smith',
+        username='john_smith',
+        password='23444',
+        email='john@smith.com',
+    ) -> User:
+        return User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            password=password,
+            email=email,
+        )
+
+    def create_recipe(  # noqa: PLR0913, PLR0917
+        self,
+        category_data=None,
+        author_data=None,
+        title='Title',
+        description='Description',
+        slug='recipe-slug',
+        preparation_time=10,
+        preparation_time_unit='minutes',
+        servings=5,
+        servings_unit='portions',
+        preparation_steps='steps',
+        preparation_steps_is_html=False,
+        is_published=True,
+    ):
+        if category_data is None:
+            category_data = {}
+
+        if author_data is None:
+            author_data = {}
+
+        return Recipe.objects.create(
+            title=title,
+            description=description,
+            slug=slug,
+            preparation_time=preparation_time,
+            preparation_time_unit=preparation_time_unit,
+            servings=servings,
+            servings_unit=servings_unit,
+            preparation_steps=preparation_steps,
+            preparation_steps_is_html=preparation_steps_is_html,
+            is_published=is_published,
+            category=self.create_category(**category_data),
+            author=self.create_author(**author_data),
+        )
--- tests/test_recipe_views.py
+++ tests/test_recipe_views.py
@@ -1,23 +1,15 @@
 from http import HTTPStatus
 
-from django.test import TestCase
 from django.urls import resolve, reverse
 
 from recipes import views
+from tests.test_recipe_base import RecipeBaseTest
 
 
-class RecipeViewsTest(TestCase):
+class RecipeViewsTest(RecipeBaseTest):
     def test_recipe_home_view_function_is_ok(self):  # noqa: PLR6301
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)  # noqa: PT009
-
-    def test_recipe_category_view_function_is_ok(self):  # noqa: PLR6301
-        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
-        self.assertIs(view.func, views.category)  # noqa: PT009
-
-    def test_recipe_detail_view_function_is_ok(self):  # noqa: PLR6301
-        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
-        self.assertIs(view.func, views.recipe)  # noqa: PT009
 
     def test_recipe_home_view_returns_status_code_OK(self):
         response = self.client.get(reverse('recipes:home'))
@@ -32,3 +24,80 @@ class RecipeViewsTest(TestCase):
         self.assertIn(  # noqa: PT009
             '<h1>Não existem receitas</h1>', response.content.decode('utf-8')
         )
+
+    def test_recipe_home_template_load_recipes(self):
+        self.create_recipe()
+
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        recipe_context = response.context['recipes']
+
+        self.assertIn('Title', content)  # noqa: PT009
+        self.assertEqual(len(recipe_context), 1)  # noqa: PT009
+
+    def test_recipe_home_template_not_published(self):
+        self.create_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:home'))
+
+        self.assertIn(  # noqa: PT009
+            '<h1>Não existem receitas</h1>', response.content.decode('utf-8')
+        )
+
+    def test_recipe_category_view_function_is_ok(self):  # noqa: PLR6301
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertIs(view.func, views.category)  # noqa: PT009
+
+    def test_recipe_category_view_returns_status_code_NOT_FOUND(self):
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': 1000})
+        )
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)  # noqa: PT009
+
+    def test_recipe_category_template_load_recipes(self):
+        test_title = 'Category Page'
+        self.create_recipe(title=test_title)
+
+        response = self.client.get(reverse('recipes:category', args=(1,)))
+        content = response.content.decode('utf-8')
+
+        self.assertIn(test_title, content)  # noqa: PT009
+
+    def test_recipe_category_template_not_published(self):
+        recipe = self.create_recipe(is_published=False)
+
+        response = self.client.get(
+            reverse(
+                'recipes:category', kwargs={'category_id': recipe.category_id}
+            )
+        )
+
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)  # noqa: PT009
+
+    def test_recipe_detail_view_function_is_ok(self):  # noqa: PLR6301
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertIs(view.func, views.recipe)  # noqa: PT009
+
+    def test_recipe_detail_view_returns_status_code_NOT_FOUND(self):
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1000})
+        )
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)  # noqa: PT009
+
+    def test_recipe_detail_template_load_recipe(self):
+        test_title = 'Detail Page - Read one recipe'
+        self.create_recipe(title=test_title)
+
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id': 1}))
+        content = response.content.decode('utf-8')
+
+        self.assertIn(test_title, content)  # noqa: PT009
+
+    def test_recipe_detail_template_not_published(self):
+        recipe = self.create_recipe(is_published=False)
+
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.id})
+        )
+
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)  # noqa: PT009
